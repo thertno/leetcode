e5098a3a4285af78b8f359314c5e228c57863c52
penglei <penglei@zhangyue.com>
2020-06-04 00:09:06 +0800

    对称二叉树

---
+++ 101.对称二叉树.java
@@ -0,0 +1,32 @@
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=101 lang=java
+ *
+ * [101] 对称二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return cmp(root, root);
+    }
+
+    public boolean cmp(TreeNode leftNode, TreeNode rightNode) {
+        if (leftNode == null && rightNode == null) {
+            return true;
+        }
+        if (leftNode == null || rightNode == null || leftNode.val != rightNode.val) {
+            return false;
+        }
+        return cmp(leftNode.left, rightNode.right) && cmp(leftNode.right, rightNode.left);
+    }
+}
+// @lc code=end
