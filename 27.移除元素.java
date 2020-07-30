aa762cca91e1ad7e00b2331c09ba0e380f05825a
penglei <penglei@zhangyue.com>
2020-07-30 15:52:29 +0800

    commit

---
+++ 27.移除元素.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=27 lang=java
+ *
+ * [27] 移除元素
+ */
+
+// @lc code=start
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int len = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[len] = nums[i];
+                len++;
+            }
+        }
+        return len;
+    }
+}
+// @lc code=end
